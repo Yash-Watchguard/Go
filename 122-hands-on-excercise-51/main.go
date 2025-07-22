c04b65a4eb31cc64ab75ebcca16007215cc92f15
Yash-Watchguard <yashgoyal322023@gmail.com>
2025-07-22 22:16:11 +0530

    22/7/2025 commit

---
+++ 122-hands-on-excercise-51/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mp := make(map[string][]string)
+
+	mp["bond_james"] = []string{"shaken", "notstirred", "martinis"}
+	mp["bond_yash"] = []string{"shaken", "notstirred", "martinis"}
+	mp["bond_manav"] = []string{"shaken", "notstirred", "martinis"}
+	fmt.Println("before deletion\n")
+	fmt.Println("%#v", mp)
+
+	delete(mp, "bond_manav")
+
+	fmt.Println("after")
+	fmt.Println("%#v", mp)
+
+	// for key, val := range mp {
+	// 	fmt.Printf("%s have given choices\n", key)
+	// 	for _, v := range val {
+	// 		fmt.Printf("1.%s\n", v)
+	// 	}
+	// 	fmt.Println("\n")
+	// }
+
+}
